96df20db11596680e8244b3f2fb9a1f4fcdab057
Jedidah <jedidahwambuguw@gmail.com>
2023-06-09 00:21:46 +0300

    Added Task 1 Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,8 +10,8 @@ def top_ten(subreddit):
     prints the titles of the first 10
     hot posts listed for a given subreddit
     """
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Custom User Agent'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "DepartureNo8863"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
