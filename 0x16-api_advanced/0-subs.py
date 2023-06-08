fcdbc3ec2fc599619acc7e734ba073b76c6ce8d3
Jedidah <jedidahwambuguw@gmail.com>
2023-06-08 23:58:34 +0300

    Added task 0 How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 
-""" A model working with Reddit API"""
+"""
+Model working with Reddit API
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
     """
-    A function that queries the Reddit API and returns the number
-    of subscribers for a given subreddit
+    function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit
     """
+    headers = {"User-Agent": 'u/DepartureNo8863'}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+        return data.get('data').get('subscribers')
     else:
         return 0
